f84efc324939399280d226546cf9b20f302aad8f
ArpitaWagulde <arpitawgulde@gmail.com>
2025-10-19 01:36:54 -0400

    Introduce branching

--- dags/titanic_dag.py
+++ dags/titanic_dag.py
@@ -1,8 +1,17 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta
 
 from src import model_development
+
+def check_accuracy(**context):
+    acc = context['ti'].xcom_pull(task_ids='evaluate_model')
+    print(f"Model accuracy is {acc:.4f}. Checking against threshold...")
+
+    if acc >= 0.80:
+        return 'end_success'
+    return 'end_failure'
 
 # Default arguments for the DAG
 default_args = {
@@ -18,6 +27,8 @@ with DAG(
     description='Titanic pipeline using PythonOperator',
     catchup=False
 ) as dag:
+    
+    start = EmptyOperator(task_id='start')
 
     preprocess_task = PythonOperator(
         task_id='preprocess_data',
@@ -34,4 +45,12 @@ with DAG(
         python_callable=model_development.evaluate_model,
     )
 
-    preprocess_task >> train_task >> evaluate_task
+    branch_task = BranchPythonOperator(
+        task_id='check_accuracy_branch',
+        python_callable=check_accuracy,
+    )
+
+    end_success = EmptyOperator(task_id='end_success')
+    end_failure = EmptyOperator(task_id='end_failure')
+
+    start >> preprocess_task >> train_task >> evaluate_task >> branch_task >> [end_success, end_failure]
